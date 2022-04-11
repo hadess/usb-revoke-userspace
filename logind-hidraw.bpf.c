129f4c7b33a814524507a16148d308b2454d7ab3
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2022-04-11 16:07:09 +0200

    Add initial implementation

---
+++ logind-hidraw.bpf.c
@@ -0,0 +1,88 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+
+/*
+ * Note: for the following code to compile, we need HIDRAW to be included
+ * in vmlinuz (CONFIG_HIDRAW=y).
+ * If HID is compiled as a separate module, we need to use the vmlinux.h
+ * which contains the various hid symbols, it can be generated through:
+ *
+ * $> ./tools/bpf/bpftool/bpftool btf dump \
+ *        file /sys/kernel/btf/hidraw format c > samples/bpf/vmlinux.h
+ *
+ * Once the code is compiled, the fact that HIDRAW is a separate module
+ * or not is not an issue, the same binary will run similarily.
+ */
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+char LICENSE[] SEC("license") = "GPL";
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 8192);
+	__type(key, __u64);
+	__type(value, u8);
+} authorized_files SEC(".maps");
+
+int foreground = 1;
+
+SEC("fentry/hidraw_open")
+int BPF_PROG(hidraw_open, struct inode *inode, struct file *file)
+{
+	pid_t pid;
+	const __u8 revoked = 0;
+	__u64 id = (__u64)file;
+
+	/* not in forgeground, don't care */
+	if (!foreground)
+		return 0;
+
+	pid = bpf_get_current_pid_tgid() >> 32;
+	bpf_printk("fentry open: pid = %d, file = %p", pid, file);
+
+	/* store the id in the allowed list */
+	bpf_map_update_elem(&authorized_files, &id, &revoked, BPF_ANY);
+
+	return 0;
+}
+
+SEC("fentry/hidraw_release")
+int BPF_PROG(hidraw_release, struct inode *inode, struct file *file)
+{
+	pid_t pid;
+	__u64 id = (__u64)file;
+
+	/* not in forgeground, don't care */
+	if (!foreground)
+		return 0;
+
+	pid = bpf_get_current_pid_tgid() >> 32;
+	bpf_printk("fentry delete: pid = %d, file = %p", pid, file);
+
+	/* the file has been closed, we can clean up */
+	bpf_map_delete_elem(&authorized_files, &id);
+
+	return 0;
+}
+
+SEC("fmod_ret/hidraw_bpf_revoked")
+int BPF_PROG(hidraw_bpf_revoked, __u64 file)
+{
+	__u8 *revoked;
+
+	/* first check if the file is in our list */
+	revoked = bpf_map_lookup_elem(&authorized_files, &file);
+
+	/* not part of our list, abort */
+	if (!revoked)
+		return 0;
+
+	/* we are not in foreground, the file is revoked */
+	if (!foreground)
+		return 1;
+
+	/* foreground, let's use the revoked state */
+	return *revoked;
+}
---
+++ logind-hidraw.c
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: (GPL-2)
+
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/resource.h>
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+#include "logind-hidraw.skel.h"
+
+static bool foreground = true;
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+{
+	return vfprintf(stderr, format, args);
+}
+
+static volatile sig_atomic_t stop;
+
+void sig_int(int signo)
+{
+	stop = 1;
+}
+
+void sig_usr(int signo)
+{
+	foreground = !foreground;
+}
+
+int main(int argc, char **argv)
+{
+	struct logind_hidraw_bpf *skel;
+	int err;
+	bool prev_state = foreground;
+
+	//libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
+	/* Set up libbpf errors and debug info callback */
+	libbpf_set_print(libbpf_print_fn);
+
+	/* Open load and verify BPF application */
+	skel = logind_hidraw_bpf__open_and_load();
+	if (!skel) {
+		fprintf(stderr, "Failed to open BPF skeleton\n");
+		return 1;
+	}
+
+	/* Attach tracepoint handler */
+	err = logind_hidraw_bpf__attach(skel);
+	if (err) {
+		fprintf(stderr, "Failed to attach BPF skeleton\n");
+		goto cleanup;
+	}
+
+	if (signal(SIGINT, sig_int) == SIG_ERR) {
+		fprintf(stderr, "can't set signal handler: %s\n", strerror(errno));
+		goto cleanup;
+	}
+
+	if (signal(SIGUSR1, sig_usr) == SIG_ERR) {
+		fprintf(stderr, "can't set signal handler: %s\n", strerror(errno));
+		goto cleanup;
+	}
+
+	printf("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` "
+	       "to see output of the BPF programs.\n");
+
+	while (!stop) {
+		sleep(0.1);
+		if (prev_state != foreground) {
+			skel->data->foreground = foreground;
+			if (!foreground) {
+				__u64 key, prev_key = -1;
+				__u8 revoked = 1;
+
+				/* revoke all currently held entries in the allowed list */
+				while(bpf_map_get_next_key(bpf_map__fd(skel->maps.authorized_files),
+							   &prev_key,
+							   &key) == 0) {
+					bpf_map_update_elem(bpf_map__fd(skel->maps.authorized_files),
+							    &key,
+							    &revoked,
+							    0);
+					prev_key = key;
+				}
+			}
+
+			printf("new state is %s\n", foreground ? "foreground" : "background");
+
+			prev_state = foreground;
+		}
+	}
+
+cleanup:
+	logind_hidraw_bpf__destroy(skel);
+	return -err;
+}
