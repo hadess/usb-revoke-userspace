d86baa625598451ba66c8cc5d7eb7d469eab7c85
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2022-04-27 10:00:30 +0200

    Fix bpf error when loading the program with system includes
    Force the bpf_printk macro to expand the fmt argument as a static const,
    to prevent the compiler to put the content of fmt into .rodata.str1.1
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>

--- logind-hidraw.bpf.c
+++ logind-hidraw.bpf.c
@@ -19,6 +19,25 @@
 
 char LICENSE[] SEC("license") = "GPL";
 
+/*
+ * work around libbpf: failed to find skeleton map '.rodata.str1.1'
+ * for older bpf_helpers.h
+ *
+ * reason explained at https://lore.kernel.org/bpf/CAEf4BzZXFwBUb=HWxfzfnA-Nq9OKpNXGa=mPqpQs2ABFPdm=uA@mail.gmail.com/
+ */
+#ifndef BPF_PRINTK_FMT_MOD
+
+#undef bpf_printk
+
+#define bpf_printk(fmt, ...)				\
+({							\
+	static const char ____fmt[] = fmt;	\
+	bpf_trace_printk(____fmt, sizeof(____fmt),	\
+			 ##__VA_ARGS__);		\
+})
+
+#endif /* BPF_PRINTK_FMT_MOD */
+
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
 	__uint(max_entries, 8192);
